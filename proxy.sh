d081d784fc78f362770872a54ebaf4be3bd7e3f3
Felipe Soares <46677612+felipesoaresti@users.noreply.github.com>
2024-09-25 08:36:00 -0300

    Update proxy.sh com dicas

--- proxy.sh
+++ proxy.sh
@@ -12,3 +12,21 @@ https_proxy=$MY_PROXY_URL
 ftp_proxy=$MY_PROXY_URL
 
 export HTTP_PROXY HTTPS_PROXY FTP_PROXY http_proxy https_proxy ftp_proxy
+
+###
+# Dicas:
+# vi /etc/dnf/dnf.conf
+# add to the end
+#proxy=meu_proxy.com:3128
+
+# for curl
+# vi ~/.curlrc
+# create new
+#proxy=meu_proxy.com:3128
+
+# for wget
+# vi /etc/wgetrc
+# add to the end
+#http_proxy = meu_proxy.com:3128
+#https_proxy = meu_proxy.com:3128
+#ftp_proxy = meu_proxy.com:3128
